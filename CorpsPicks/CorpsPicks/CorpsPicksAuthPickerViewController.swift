ae3fe66c661049f1d7e85bcfbe1fee9dadf6c76a
vfxdrummer <timothy.brandt@gmail.com>
2017-05-13 06:37:18 -0700

    Add icon

---
+++ CorpsPicks/CorpsPicks/CorpsPicksAuthPickerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CorpsPicksAuthPickerViewController.swift
+//  CorpsPicks
+//
+//  Created by Timothy Brandt on 5/9/17.
+//  Copyright © 2017 Tim Brandt. All rights reserved.
+//
+
+import Foundation
